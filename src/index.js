6cf5c3ccacae4a5cef5107dccf16c6432dfc6343
github username <lluisguardiola.h@gmail.com>
2020-01-09 11:48:52 -0600

    added fetch toys to main func

--- src/index.js
+++ src/index.js
@@ -1,30 +1,33 @@
 let addToy = false
+function main(){
+  document.addEventListener("DOMContentLoaded", ()=>{
+    const addBtn = document.querySelector('#new-toy-btn')
+    const toyForm = document.querySelector('.container')
+    addBtn.addEventListener('click', () => {
+      // hide & seek with the form
+      addToy = !addToy
+      if (addToy) {
+        toyForm.style.display = 'block'
+      } else {
+        toyForm.style.display = 'none'
+      }
+    })
 
-document.addEventListener("DOMContentLoaded", ()=>{
-  const addBtn = document.querySelector('#new-toy-btn')
-  const toyForm = document.querySelector('.container')
-  addBtn.addEventListener('click', () => {
-    // hide & seek with the form
-    addToy = !addToy
-    if (addToy) {
-      toyForm.style.display = 'block'
-    } else {
-      toyForm.style.display = 'none'
-    }
+    fetchToys();
   })
-
-})
+}
 
 function fetchToys() {
   return fetch('http://localhost:3000/toys')
     .then( resp => resp.json())
-    .then( addToys )
+    .then( json => addToys(json) )
 }
 
 function addToys(json) {
   const toyDiv = document.querySelector("div#toy-collection");
-  const toys = json["message"];
-  for (const toy in toys ) {
-    
-  }
+  const toys = json;
+
+  console.log(toys);
 }
+
+main();
